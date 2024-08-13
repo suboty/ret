852171baaa371cc779b7f15c6d9c9b98a11544dd
suboty <seed.suboty@gmail.com>
2024-08-13 13:29:32 +0300

    add arg parser for debugging

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import pprint
+import argparse
+
 from src.translator import Translator
 from src.logger import logger
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Translate regular expression')
+    parser.add_argument('-r', '--regex', type=str, help='input regex')
+    parser.add_argument('-i', '--input', nargs='?', type=str, default='python', help='input syntax of input regex')
+    parser.add_argument('-o', '--output', nargs='?', type=str, default='python', help='output syntax')
+
+    args = parser.parse_args()
 
     translator = Translator()
 
-    input_regex = 'abc(d|e)'
+    input_regex = args.regex
 
-    logger.info(translator.translate(string=input_regex, input_syntax='python', output_syntax='python'))
-    logger.info(translator.ast)
+    input_syntax = args.input
+    output_syntax = args.output
+
+    output_regex = translator.translate(string=input_regex, input_syntax=input_syntax, output_syntax=output_syntax)
+
+    logger.info(f'Get regex in <{input_syntax}> input syntax: {input_regex}')
+    logger.info(f'Get Regex in <{output_syntax}> output syntax: {output_regex}')
+    logger.info(f'Get AST:\n{pprint.pformat(translator.ast)}')
