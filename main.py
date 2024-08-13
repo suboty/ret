0a4202ee4307baba3721c9026370b148223d3967
suboty <seed.suboty@gmail.com>
2024-08-13 10:55:13 +0300

    remove ast optimization from src

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from src.ast_optimization.population_generating import PopulationGenerator
 from src.translator import Translator
 from src.logger import logger
 
 if __name__ == '__main__':
-    population_generator = PopulationGenerator(
-        number_of_individual=10,
-        number_of_iterations=10
-    )
 
     translator = Translator()
 
