93a5c49116f1742d84dcba9bf778f0040a66ea6b
suboty <seed.suboty@gmail.com>
2024-08-27 18:39:58 +0300

    add population algorithms optimizing output for new regex

--- main.py
+++ main.py
@@ -60,34 +60,41 @@ if __name__ == '__main__':
     )
     logger.info('\nAS IS'
                 f'\n--- Performance metric = <{as_is_performance_metric}> sec'
-                f'\n--- AS IS AST:\n{get_pretty_ast(translator.ast)}')
+                f'\n--- AS IS AST:\n{get_pretty_ast(translator.ast)}'
+                f'\n--- AS IS REGEX:\n{output_regex}')
 
     # DE
+    de_regex = python_generator(ast=de_optimizing_ast)
     de_performance_metric = get_performance_metric(
-        regex=python_generator(ast=de_optimizing_ast),
+        regex=de_regex,
         syntax=1,
         n_iter=500
     )
     logger.info('\nDE'
                 f'\n--- Performance metric = <{de_performance_metric}> sec'
-                f'\n--- TO BE DE AST:\n{get_pretty_ast(de_optimizing_ast)}')
+                f'\n--- TO BE DE AST:\n{get_pretty_ast(de_optimizing_ast)}'
+                f'\n--- TO BE DE REGEX:\n{de_regex}')
 
     # FSS
+    fss_regex = python_generator(ast=fss_optimizing_ast)
     fss_performance_metric = get_performance_metric(
-        regex=python_generator(ast=fss_optimizing_ast),
+        regex=fss_regex,
         syntax=1,
         n_iter=500
     )
     logger.info('\nFSS'
                 f'\n--- Performance metric = <{fss_performance_metric}> sec'
-                f'\n--- TO BE FSS AST:\n{get_pretty_ast(fss_optimizing_ast)}')
+                f'\n--- TO BE FSS AST:\n{get_pretty_ast(fss_optimizing_ast)}'
+                f'\n -- TO BE FSS REGEX:\n{fss_regex}')
 
     # PSO
+    pso_regex = python_generator(ast=pso_optimizing_ast)
     pso_performance_metric = get_performance_metric(
-        regex=python_generator(ast=pso_optimizing_ast),
+        regex=pso_regex,
         syntax=1,
         n_iter=500
     )
     logger.info('\nPSO'
                 f'\n--- Performance metric = <{pso_performance_metric}> sec'
-                f'\n--- TO BE PSO AST:\n{get_pretty_ast(pso_optimizing_ast)}')
+                f'\n--- TO BE PSO AST:\n{get_pretty_ast(pso_optimizing_ast)}'
+                f'\n--- TO BE PSO REGEX:\n{pso_regex}')
